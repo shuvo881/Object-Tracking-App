55aede83ae19dbc0444762f209e905a0509ca72c
shuvoDAI <mostofa@devolveai.com>
2025-02-06 10:31:22 +0600

    main update for train code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gui.main import create_app
 from model_train import train 
 
 def main():
-    # train.main()
+    train.main()
     create_app(model_path='runs/detect/train/weights/best.pt')
 
 if __name__ == "__main__":
